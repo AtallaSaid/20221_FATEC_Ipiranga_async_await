efe47ee2a4f4da3a59cff0e1c74157320654a1c1
Atalla Said <87430952+AtallaSaid@users.noreply.github.com>
2022-03-16 17:33:56 -0300

    Add files via upload
    Continuação da aula de PAOO 16 03 2022

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 require('dotenv').config()
 const axios = require ('axios')
 
-console.log(process.env.APP_KEY)
+const { PROTOCOL, BASE_URL, APPID, UNITS, CNT, Q, LANGUAGE} = process.env
+
+const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&lang=${LANGUAGE}&units=${UNITS}`
+
+
+axios
+    .get(url)
+    .then(res => {
+        //console.log(res)
+        return res.data
+    })
+    .then(res => {
+        return res['list']
+    })
+    .then(res => {
+        console.log(res)
+    })
+    
+    .then(res => {
+        //for(let it = 0; i < res.length; i ++){
+          for (previsao of res){
+              console.log(`
+              Temp min: ${previsao.main.temp_min},
+              Temp max: ${previsao.main.temp_max},
+                Descrição: ${previsao.weather[0].description}
+              `)
+          }  
+        })
+
 
